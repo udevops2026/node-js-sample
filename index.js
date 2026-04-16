5caf2e596aae26411454de8d60eee284b7317362
udevops2026 <udevops2026@gmail.com>
2026-04-17 01:40:12 +0530

    Update index.js
    Signed-off-by: udevops2026 <udevops2026@gmail.com>

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 var express = require('express')
 var app = express()
 
-app.set('port', process.env.PORT || 5000)
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-  res.send(`
-    <html>
-    <head><title>Node.js CI/CD Demo</title></head>
-    <body>
-      <h1>🚀 Node.js CI/CD Demo Successful</h1>
-      <h2>Deployed using Jenkins + PM2 + AWS EC2</h2>
-      <p>Congratulations! Your pipeline is working.</p>
-    </body>
-    </html>
-  `)
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(5000, () => {
+    console.log("App running on port 5000")
 })
